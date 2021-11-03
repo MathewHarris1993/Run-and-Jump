6507c1beca82b6723639c921416476e4d438a780
Mathew Harris <10959101@uvu.edu>
2021-11-02 18:37:25 -0600

    Jump
    Coded in the jump action and tied it to the spacebar.

--- Run and Jump/Assets/Scripts/PlayerController.cs
+++ Run and Jump/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //set player rigidbody component variable
+    private Rigidbody playerRb;
+
     void Start()
     {
+        //tie the playerRb component variable to component 
+        playerRb = GetComponent<Rigidbody>();
+        
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
